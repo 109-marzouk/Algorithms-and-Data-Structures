5f6f35f6ae4f0b32f1087312b6c8bd7331200353
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-09-02 10:09:40 +0200

    Sorting Algorithms

--- Algorithms/Sorting Algorithms/Merge Sort.cs
+++ Algorithms/Sorting Algorithms/Merge Sort.cs
@@ -1,35 +1,56 @@
 using System;
 
 class MainClass {
+
   static void merge(int[] arr, int leftIndex, int endLeftIndex, int endRightIndex){
+    // leftArr [leftIndex ... endLeftIndex]
+    // rightArr [endLeftIndex + 1 ...  endRightIndex]
     int leftArrLength = endLeftIndex - leftIndex + 1;
     int rightArrLength = endRightIndex - endLeftIndex;
+
     int[] leftArr = new int[leftArrLength], rightArr = new int[rightArrLength];
 
-    for(int i = 0; i < leftArrLength; i++) leftArr[i] = arr[leftIndex + i];
-    for(int j = 0; j < rightArrLength; j++) rightArr[j] = arr[endLeftIndex + j + 1];
+    // Copying the data from the parent Array to the leftArr & rightArr
+    for(int i = 0; i < leftArrLength; i++)
+      leftArr[i] = arr[leftIndex + i];
+    for(int j = 0; j < rightArrLength; j++)
+      rightArr[j] = arr[endLeftIndex + j + 1];
 
+    // the start index of the array that will be merged
     int mergedArrIndex = leftIndex;
+
+    // set two start pointers for both left and right arrays
     int leftIndexMerge = 0, rightIndexMerge = 0;
+
+    // the merging operation
     while(leftIndexMerge < leftArrLength && rightIndexMerge < rightArrLength){
-      if(leftArr[leftIndexMerge] < rightArr[rightIndexMerge]) arr[mergedArrIndex++] = leftArr[leftIndexMerge++];
-      else arr[mergedArrIndex++] = rightArr[rightIndexMerge++];
+      if(leftArr[leftIndexMerge] < rightArr[rightIndexMerge])
+        arr[mergedArrIndex++] = leftArr[leftIndexMerge++];
+      else
+        arr[mergedArrIndex++] = rightArr[rightIndexMerge++];
     }
-    while(leftIndexMerge < leftArrLength) arr[mergedArrIndex++] = leftArr[leftIndexMerge++];
-    while(rightIndexMerge < rightArrLength) arr[mergedArrIndex++] = rightArr[rightIndexMerge++];
+
+    // copying the rest elements
+    while(leftIndexMerge < leftArrLength)
+      arr[mergedArrIndex++] = leftArr[leftIndexMerge++];
+
+    while(rightIndexMerge < rightArrLength)
+      arr[mergedArrIndex++] = rightArr[rightIndexMerge++];
   }
 
-  static void mergeSort(int[] arr, int leftIndex, int rightIndex){
-    if(leftIndex < rightIndex){
-      int m = leftIndex + (rightIndex - 1) / 2;
+  static void mergeSort(int[] arr, int leftIndex, int endRightIndex){
+    if(leftIndex < endRightIndex){
+      // leftArr [leftIndex ... m]
+      // rightArr [m + 1 ...  endRightIndex]
+      int m = leftIndex + (endRightIndex - leftIndex) / 2;
       mergeSort(arr, leftIndex, m);
-      mergeSort(arr, m + 1, rightIndex);
-      merge(arr, leftIndex, m, rightIndex);
+      mergeSort(arr, m + 1, endRightIndex);
+      merge(arr, leftIndex, m, endRightIndex);
     }
   }
   public static void Main (string[] args) {
     int[] arr = {1,2,5,10,1,2,3,4,4};
-    mergeSort(arr, 0, arr.Length - 1);
+    mergeSort(arr, 0, arr.Length-1);
     foreach(int i in arr) Console.WriteLine(i);
   }
 }
