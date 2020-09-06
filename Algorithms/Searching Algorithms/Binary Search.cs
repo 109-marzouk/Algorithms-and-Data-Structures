205235f85e5d518f5d4aee0d7ce2ad76eeea069d
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-09-06 23:46:51 +0200

    Searching Algorithms

---
+++ Algorithms/Searching Algorithms/Binary Search.cs
@@ -0,0 +1,9 @@
+/*
+* Author: Mohamed Marzouk
+* -------------------------
+* Binary Search Algorithm:
+* -------------------------
+* Worst Case Time Complixty: O(log(N))
+* Space Complixty: O(N) [in case of recursive implementation] / O(1) [in case of iterative implementation]
+* Used Language: C#
+*/
