c4b5ecff259aba57b689767829bb53e2449a53e5
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-09-09 00:11:27 +0200

    Searching Algorithms

--- Algorithms/Searching Algorithms/Ternary Search.js
+++ Algorithms/Searching Algorithms/Ternary Search.js
@@ -23,5 +23,23 @@ function RecursiveTernarySearch(arr, left, right, target){
   return -1;
 }
 
+function IterativeTernarySearch(arr, left, right, target){
+  while(right >= left){
+    let firstMid = left + Math.floor((right - left) / 3);
+    let secondMid = firstMid + Math.floor((right - left) / 3);
+    if(arr[firstMid] == target) return firstMid;
+    if(arr[secondMid] == target) return secondMid;
+    if(target < arr[firstMid])
+      right = firstMid - 1;
+    else if(target > arr[secondMid])
+      left = secondMid + 1;
+    else{
+      left = secondMid + 1;
+      right = firstMid - 1;
+    }
+  }
+  return -1;
+}
 let arr = [1, 2, 3, 4, 5, 6, 15, 22];
 console.log(RecursiveTernarySearch(arr, 0, arr.length - 1, 15));
+console.log(IterativeTernarySearch(arr, 0, arr.length - 1, 22));
