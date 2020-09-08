ff85e9ec2d60b657e12cc93778a731cb97f0085f
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-09-08 23:30:10 +0200

    Searching Algorithms

--- Algorithms/Searching Algorithms/Ternary Search.js
+++ Algorithms/Searching Algorithms/Ternary Search.js
@@ -7,3 +7,20 @@
 * Space Complixty: O(N) [in case of recursive implementation] / O(1) [in case of iterative implementation]
 * Used Language: JavaScript
 */
+function RecursiveTernarySearch(arr, left, right, target){
+  if(right > left){
+    let firstMid = left + (right - left) / 3;
+    let secondMid = firstMid + (right - left) / 3;
+    if(arr[firstMid] == target) return firstMid;
+    if(arr[secondMid] == target) return secondMid;
+    if(target < arr[firstMid])
+      return RecursiveTernarySearch(arr, left, firstMid - 1, target);
+    else if(target > arr[secondMid])
+      return RecursiveBinarySearch(arr, secondMid + 1, right, target);
+    else
+      return RecursiveBinarySearch(arr, firstMid + 1, secondMid - 1, target);
+  }
+  return -1;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 15, 22];
