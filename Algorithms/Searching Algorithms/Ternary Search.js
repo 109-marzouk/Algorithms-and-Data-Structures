613b192d873c9a88223d40318ac82ac66f3fafe2
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-09-08 23:18:24 +0200

    Searching Algorithms

---
+++ Algorithms/Searching Algorithms/Ternary Search.js
@@ -0,0 +1,9 @@
+/*
+* Author: Mohamed Marzouk
+* -------------------------
+* Ternary Search Algorithm:
+* -------------------------
+* Worst Case Time Complixty: O(log3(N))
+* Space Complixty: O(N) [in case of recursive implementation] / O(1) [in case of iterative implementation]
+* Used Language: JavaScript
+*/
