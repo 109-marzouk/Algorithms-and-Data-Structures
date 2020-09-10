fdc757679676866ca49b5bd97cb1176e186887ca
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-09-10 23:23:37 +0200

    Sorting Algorithms

--- Algorithms/Sorting Algorithms/Shell Sort.cpp
+++ Algorithms/Sorting Algorithms/Shell Sort.cpp
@@ -11,3 +11,28 @@
 *--------------------
 * not stable.
 */
+#include <iostream>
+using namespace std;
+void shellSort(int arr[], int n) {
+  for(int gap = n / 2; gap > 0; gap /= 2){
+    for(int i = gap; i < n; i++){
+      int temp = arr[i];
+      int j;
+      for(j = i; j >= gap && arr[j - gap] > temp; j -= gap){
+        arr[j] = arr[j - gap];
+      }
+      arr[j] = temp;
+    }
+  }
+}
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) cout << array[i] << " ";
+  cout << endl;
+}
+int main() {
+  int arr[] = {8, 2, 3, 4, 1, 5, 9};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  shellSort(arr, size);
+  cout << "Sorted array: \n";
+  printArray(arr, size);
+}
