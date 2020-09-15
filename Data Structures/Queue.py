8e29a4bbd9ffe7a4faa9cb119bc76971992bac15
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-09-15 22:31:27 +0200

    Data Structures

---
+++ Data Structures/Queue.py
@@ -0,0 +1,34 @@
+"""
+* Author: Mohamed Marzouk
+* ----------------------------------------
+* Queue:
+* ----------------------------------------
+* Time Complixty: O(1)
+* Space Complixty: O(N)
+* Used Language: Python
+"""
+class Queue:
+    def __init__(self): self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if len(self.queue) < 1: return None
+        return self.queue.pop(0)
+
+    def display(self): print(self.queue)
+
+    def size(self): return len(self.queue)
+
+q = Queue()
+q.enqueue(3)
+q.enqueue(2)
+q.enqueue(5)
+q.enqueue(4)
+
+q.display()
+
+q.dequeue()
+print("After removing an element")
+q.display()
